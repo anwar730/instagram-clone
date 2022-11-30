81cad7abcb9d26b340340d737f591f795141ad0a
anwar <anwaarismail449@gmail.com>
2022-11-30 14:57:33 +0300

    Add the relationships between the tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :comments, through: :posts
+    has_many :events
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
