a15be935297c24eb14cc2437d049f4242fc4f1b0
anwar <anwaarismail449@gmail.com>
2022-11-30 15:00:36 +0300

    Add the various relevant serializers

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :image, :caption, :user_id, :likes
+  
+  belongs_to :user
+  has_many :comments
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :password_digest, :profile_pic, :followers
+
+  has_many :posts
 end
