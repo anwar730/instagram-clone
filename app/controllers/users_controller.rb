c119cf844adeae484c779a989c960e56d66d149b
anwar <anwaarismail449@gmail.com>
2022-11-30 14:56:10 +0300

    Add the crud functionalities of the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+        skip_before_action :authorize, only: [:create]
+    
+        def create
+          user = User.create!(user_params)
+          session[:user_id] = user.id
+          render json: user, status: :created
+        end
+      
+        def show
+          render json: @current_user
+        end
+    
+        def update
+          if @current_user
+          @current_user.update!(update_params)
+          render json: @current_user, status: :accepted
+          end
+        end
+      
+        private
+      
+        def user_params
+          params.permit(:username, :email, :password, :password_confirmation, :profile_pic, :followers)
+        end
+    
+        def update_params
+          params.permit(:username, :email, :profile_pic)
+        end
 end
