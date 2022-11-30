0d5f3c2c456f359d9be4dae705237f316e4310b4
anwar <anwaarismail449@gmail.com>
2022-11-30 15:01:12 +0300

    Add the crud functionalities of the post component

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+    skip_before_action :authorize, only: [:create, :index]
+  
+      def index
+          post = Post.all
+          post.order(created_at: :desc)
+          render json: post
+        end
+  
+        def create
+          posts = Post.create!(post_params)
+          render json: posts, status: :created
+        end
+  
+        def update
+          post = Post.find_by(id: params[:id])
+          post.update!(edit_params)
+          render json: post
+        end
+  
+        private
+  
+        def post_params
+          params.permit(:image, :caption, :user_id, :likes)
+        end
+  
+        def edit_params
+          params.permit(:likes)
+        end
 end
+  
